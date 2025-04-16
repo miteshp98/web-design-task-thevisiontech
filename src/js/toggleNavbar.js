3a87638cc414fdff9ec0e25d25a7e271ddf2af2c
miteshp98 <miteshpanchal3d@gmail.com>
2025-04-17 02:27:51 +0530

    init test project (add header - home page (hero section - category - featured products - details) styles & layout & static structure)

---
+++ src/js/toggleNavbar.js
@@ -0,0 +1,24 @@
+function toggleNav() {
+  const body = document.querySelector('body');
+  const overlay = body.querySelector('.bg-overlay');
+
+  body.addEventListener('click', function (e) {
+    const navOpen = e.target.closest('.header__action-btn--menu');
+    const navClose = e.target.closest('.navbar__close-btn');
+    const navbar = this.querySelector('.navbar');
+
+    if (navOpen) {
+      navbar.classList.add('navbar--toggle');
+      overlay.classList.add('bg-overlay--active');
+      overlay.removeAttribute('hidden');
+    }
+
+    if (navClose) {
+      navbar.classList.remove('navbar--toggle');
+      overlay.classList.remove('bg-overlay--active');
+      overlay.setAttribute('hidden', true);
+    }
+  });
+}
+
+toggleNav();
